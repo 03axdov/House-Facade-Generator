d412f2d9a1d959e7e45dfad93a4fc678e666e947
Axel Dovskog <axeld@LenovoAxel.localdomain>
2022-10-10 20:53:44 +0200

    Implemented the generator

--- data_processing.py
+++ data_processing.py
@@ -8,12 +8,25 @@ import matplotlib.pyplot as plt
 from utils import *
 
 
+dataset_name = "facades"
+URL = f"http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/{dataset_name}.tar.gz"
+
+path_to_zip = tf.keras.utils.get_file(
+    fname=f"{dataset_name}.tar.gz",
+    extract=True,
+    origin=URL
+)
+
+path_to_zip = Path(path_to_zip)
+PATH = path_to_zip.parent/dataset_name
+
+
 def load_input_target_images(image_file):
 
     image = tf.io.read_file(image_file)
     image = tf.io.decode_jpeg(image)
 
-    w = image.shape[1]
+    w = tf.shape(image)[1]
     w = w // 2
     input_image = image[:, w:, :]
     target_image = image[:, :w, :]
@@ -24,46 +37,62 @@ def load_input_target_images(image_file):
     return input_image, target_image
 
 
-def load_data(debug = False):
+def debugging():
 
-    dataset_name = "facades"
-    URL = f"http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/{dataset_name}.tar.gz"
+    # print(list(PATH.parent.iterdir()))
 
-    path_to_zip = tf.keras.utils.get_file(
-        fname=f"{dataset_name}.tar.gz",
-        extract=True,
-        origin=URL
-    )
+    sample_image = tf.io.read_file(str(PATH / "train/1.jpg"))
+    sample_image = tf.io.decode_jpeg(sample_image)
+    print(sample_image.shape)   # (256, 512, 3) --> Consists of two (256,256,3) images
 
-    path_to_zip = Path(path_to_zip)
-    PATH = path_to_zip.parent/dataset_name
+    # plt.figure()
+    # plt.imshow(sample_image)
+    # plt.show()
 
-    if debug:
-        # print(list(PATH.parent.iterdir()))
+    input_image, target_image = load_input_target_images(str(PATH / "train/47.jpg"))
 
-        sample_image = tf.io.read_file(str(PATH / "train/1.jpg"))
-        sample_image = tf.io.decode_jpeg(sample_image)
-        print(sample_image.shape)   # (256, 512, 3) --> Consists of two (256,256,3) images
+    # plt.figure()
+    # plt.subplot(1,2,1)
+    # plt.imshow(input_image / 255.0)
+    # plt.title("Input")
+    # plt.axis("off")
 
-        # plt.figure()
-        # plt.imshow(sample_image)
-        # plt.show()
+    # plt.subplot(1,2,2)
+    # plt.imshow(target_image / 255.0)
+    # plt.title("Target")
+    # plt.axis("off")
 
-        input_image, target_image = load_input_target_images(str(PATH / "train/47.jpg"))
+    # plt.show()
 
-        plt.figure()
-        plt.subplot(1,2,1)
-        plt.imshow(input_image / 255.0)
-        plt.title("Input")
+    plt.figure(figsize=(8, 8))
+    for i in range(0, 8, 2):
+        rj_inp, rj_target = random_jitter(input_image, target_image)
+        plt.subplot(4,2, i+1)
+        plt.imshow(rj_inp / 255.0)
         plt.axis("off")
 
-        plt.subplot(1,2,2)
-        plt.imshow(target_image / 255.0)
-        plt.title("Target")
+        plt.subplot(4,2, i+2)
+        plt.imshow(rj_target / 255.0)
         plt.axis("off")
+    
+    plt.show()
 
-        plt.show()
+
+def load_image_train(image_path):
+    input_image, target_image = load_input_target_images(image_path)
+    input_image, target_image = random_jitter(input_image, target_image)
+    input_image, target_image = normalize(input_image, target_image)
+
+    return input_image, target_image
+
+
+def load_image_test(image_path):
+    input_image, target_image = load_input_target_images(image_path)
+    input_image, target_image = resize(input_image, target_image, IMG_HEIGHT, IMG_WIDTH)
+    input_image, target_image = normalize(input_image, target_image)
+
+    return input_image, target_image
 
 
 if __name__ == "__main__":
-    load_data(debug=True)
+    debugging()
--- main.py
+++ main.py
@@ -2,3 +2,53 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from data_processing import load_image_train, load_image_test, PATH
+from models import downsample, upsample, Generator
+
+
+def main():
+
+    BUFFER_SIZE = 400
+    BATCH_SIZE = 1  # Seemingly produces ideal results in the original paper
+
+    train_dataset = tf.data.Dataset.list_files(str(PATH / "train/*.jpg"))
+    train_dataset = train_dataset.map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
+    train_dataset.shuffle(BUFFER_SIZE)
+    train_dataset.batch(BATCH_SIZE)
+
+    try:
+        test_dataset = tf.data.Dataset.list_files(str(PATH / "test/*.jpg"))
+    except tf.errors.InvalidArgumentError:
+        test_dataset = tf.data.Dataset.list_files(str(PATH / "val/*.jpg"))
+
+    test_dataset = test_dataset.map(load_image_test, num_parallel_calls=tf.data.AUTOTUNE)
+    test_dataset.batch(BATCH_SIZE)
+
+    down_model = downsample(3, 4)
+
+    for input, target in train_dataset.take(1):
+        break
+
+    down_result = down_model(tf.expand_dims(input, 0))
+    print(f"down_result.shape: {down_result.shape}")    # (1, 128, 128, 3)
+
+    up_model = upsample(3,4)
+    up_result = up_model(down_result)
+    print(f"up_result.shape: {up_result.shape}")    # (1, 256, 256, 3)
+
+    generator = Generator()
+    # tf.keras.utils.plot_model(generator, show_shapes=True, dpi=64)
+
+    plt.imshow(input)
+    plt.show()
+
+    gen_output = generator(input[tf.newaxis, ...], training=False)
+    plt.imshow(gen_output[0, ...])
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
